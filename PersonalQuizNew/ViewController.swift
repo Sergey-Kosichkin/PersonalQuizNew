0e7f08693e321342956949fe10f2dafefce34206
Sergey Kosichkin <d978827@icloud.com>
2022-12-09 08:26:07 +0300

    Initial Commit

---
+++ PersonalQuizNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuizNew
+//
+//  Created by Sergey Kosichkin on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
